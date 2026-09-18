e4a24d29b82017ce4c7bc8ab3c468c19af93d298
Utkarsh Pandey <utkarshpandey.up.2004@gmail.com>
2026-09-18 15:59:16 +0530

    [1614] [Time Beats: 100.00%] [Memory Beats: 24.75%] - LeetPush

---
+++ DSA/1614-Maximum-Nesting-Depth-of-the-Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt=0;
+        int maxi=0;
+        for(char c:s){
+            if(c=='(') {
+                cnt++;
+                maxi=max(maxi,cnt);
+          
+            }
+            else if(c==')') cnt--;
+        }
+        return maxi;
+    }
+};
