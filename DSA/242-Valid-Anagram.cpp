f2e41351e97989d8b4207c61aef3bbbfc25ae221
Utkarsh Pandey <utkarshpandey.up.2004@gmail.com>
2026-09-18 15:11:42 +0530

    [242] [Time Beats: 100.00%] [Memory Beats: 48.34%] - LeetPush

--- DSA/242-Valid-Anagram.cpp
+++ DSA/242-Valid-Anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!=t.size()) return false;
+        vector<int> chari(26,0);
+        for(char a:s){
+            chari[a-'a']++;
+        }
+        for( char b:t){
+            if(!chari[b-'a']) {
+                return false;
+            }
+            chari[b-'a']--;
+        }
+        return true;
     }
 };
